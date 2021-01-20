e9d761d5191ca2c250c8c8e828ddf213ccbc6fd4
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-20 19:08:59 +0600

    Classes created in all pages for route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <About></About>
+      <Contact></Contact>
+      <Portfolio></Portfolio>
     </div>
   );
 }
