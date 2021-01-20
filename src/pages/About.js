0755e20dfed948ad3ac611f54b43e6bbcd56e887
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-20 20:26:57 +0600

    taste case

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,7 @@ class About extends Component {
     render() {
         return (
             <div>
-                <h1 style = {{color: 'white', backgroundColor:'green'}}>About</h1>
+                <h1 style = {{color: 'white', backgroundColor:'green', margin:'40px'}}>About</h1>
             </div>
         );
     }
