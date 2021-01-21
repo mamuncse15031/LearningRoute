7483652ea413a01efb5c437f58841d6803eabd2b
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-21 09:44:25 +0600

    First SASS

--- src/pages/SassLearn.js
+++ src/pages/SassLearn.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import SassLearn from './SassLearn.scss'
+
 
 class SassLearn extends Component {
     render() {
         return (
             <div>
-                
+                <h1 className={SassLearn.TextStyle1}>Hi I am SASS</h1>
             </div>
         );
     }
