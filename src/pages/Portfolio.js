1201ac0e83a6c33fe2eafee44a9f14cd11f01973
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-21 08:51:21 +0600

    CSS using modules

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import MyCss from './MyCss.module.css'
 
 class Portfolio extends Component {
     render() {
         return (
             <div>
-                <h1>Portfolio</h1>
+                <h1 className={MyCss.TextStyle}>Portfolio</h1>
             </div>
         );
     }
