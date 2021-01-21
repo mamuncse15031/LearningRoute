fdc18e48c9121891f45a2b4b445aae2c9ee45fdf
Md. Abdullah Al Mamun <46086604+mamuncse15031@users.noreply.github.com>
2021-01-21 08:49:02 +0600

    External css as class

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style="TextStyle">Mamun</h1>
+      <h1 className="TextStyle">Home Page</h1>
     </div>
   );
 }
